af61f37e19583c64c4f487efc98925c011ff8c54
Ramit Sarkar <ramitsarkar@Ramits-MacBook-Air.local>
2023-10-08 16:36:25 +0530

    add server deploy url

--- client/src/apis/helper.ts
+++ client/src/apis/helper.ts
@@ -1,2 +1,3 @@
 // export const api_url = import.meta.env.VITE_API_URL
-export const api_url = "http://localhost:9000/api"
+export const test_url = "http://localhost:9000/api";
+export const api_url = "https://outrageous-boa-peplum.cyclic.app/api";
